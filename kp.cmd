018428bbb4ff366feb903d1fec7871dce2283ce3
Ad04a <andrey.todorov04@gmail.com>
2025-12-02 12:33:16 +0200

    adding platform independant setting

--- kp.cmd
+++ kp.cmd
@@ -1,8 +1,14 @@
 @echo off
 setlocal
 
-set BASH="C:\Program Files\Git\bin\bash.exe"
-set EXE=Build\KP_PE.exe
+if exist kp_settings.txt (
+    for /f "tokens=1,2 delims==" %%A in (kp_settings.txt) do (
+        set %%A=%%B
+    )
+) else (
+    echo Settings not found. Run: kp prepare
+    exit /b 1
+)
 
 rem === Route commands ===
 
@@ -10,6 +16,7 @@ if "%1"=="" goto do_execute
 if "%1"=="build" goto do_make
 if "%1"=="clean" goto do_clean
 if "%1"=="cleanbuild" goto do_cleanbuild
+if "%1"=="prepare" goto do_prepare
 
 if "%1"=="run" goto do_run
 
@@ -50,3 +57,12 @@ exit /b
 del /f /q "%EXE%"
 %BASH% --noprofile --norc -lc "make build"
 goto do_run
+
+:do_prepare
+if exist kp_settings.txt (
+    echo "Settings file (kp_settings.txt) already exists!"
+) else (
+    echo "BASH=enter the path to your GitBash"> kp_settings.txt
+    echo "EXE=Build\KP_PE.exe" >> kp_settings.txt
+)
+
